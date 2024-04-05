e8ffd7f3e05de8146b6120e3df0a7a0d4b911664
Andersem Reis <andersem.reis@gmail.com>
2024-04-04 21:27:34 -0400

    atualizando jogo da velha

---
+++ 1 modulo/js/jogoDaVelha.js
@@ -0,0 +1,100 @@
+let tabuleiro
+let board
+let aviso
+let jogador
+let linha
+let coluna
+
+
+function iniciar(){
+    tabuleiro = []
+    board = document.getElementById('board')
+    aviso = document.getElementById('aviso')
+    jogador = 1
+
+    for (let i = 0; i < 3; i++){
+        tabuleiro[i] = []
+        for(let j = 0; j < 3; j++){
+            tabuleiro[i][j] = 0
+        }
+
+    }
+    console.table(tabuleiro)
+    exibir()
+}
+
+function exibir(){
+    let tabela = '<table cellpadding="10" border = "1">'
+
+    for (let i = 0; i < 3; i++){
+        
+        tabela += '<tr>'
+        
+        for (let j = 0; j < 3; j++) {
+            switch(tabuleiro[i][j]){
+                case -1: marcador = 'X';break;
+                case 1: marcador = '0';break;
+                default: marcador='_'
+            }
+            tabela += '<td>' + marcador + '</td>'
+        }   
+            tabela += '</tr>'
+    }
+    tabela +='</table>'
+
+    board.innertHTML = tabela
+
+}
+
+function jogar(){
+    aviso.innertHTML = 'Vez do jogador: ' + numeroJogador()
+
+    linha = document.getElementById('linha').value - 1
+    coluna = document.getElementById('coluna').value - 1
+
+    if (tabuleiro[linha][coluna] == 0) {
+        tabuleiro[linha][coluna] = numeroJogador() == 1 ? 1 : -1
+    } else{
+        aviso.innertHTML = 'Esse campo já foi marcado'
+    }
+
+    tabuleiro[linha][coluna]= 'X'
+    console.log(linha,coluna);
+    jogador++
+    exibir()
+    checar()
+}
+
+function checar(){
+    for (let i = 0; i < 3; i++){
+        let soma = 0
+        soma = tabuleiro[i][0] + tabuleiro[i][1] + tabuleiro[i][2]
+
+        if (soma == 3 || soma == -3){
+            aviso.innertHTML = 'O jogador ' + numeroJogador() + 'ganhou!'
+
+        }
+    }
+
+    for (let i = 0; i < 3; i++){
+        let soma = 0
+        soma = tabuleiro[0][i] + tabuleiro[1][i] + tabuleiro[2][i]
+
+        if (soma == 3 || soma == -3){
+            aviso.innertHTML = 'O jogador ' + numeroJogador() + 'ganhou!'
+             
+        }
+    }
+
+    soma = tabuleiro[0][0] + tabuleiro[1][1] + tabuleiro[2][2]
+
+    if (soma == 3 || soma == -3){
+        aviso.innertHTML = 'O jogador ' + numeroJogador() + 'ganhou!'
+            
+    }
+
+}
+
+function numeroJogador(){
+    return jogador%2 + 1
+}
